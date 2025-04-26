d76afcf12ad53cadb4c1057663780ee2febad670
maheenfayyaz <princessfayyaz7@gmail.com>
2025-04-27 02:30:11 +0500

    Completed SignUp LogIn

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -15,6 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://signup-login-sooty.vercel.app/',
   ],
   methods:['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
